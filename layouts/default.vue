36a141668844277d83b9a3775aa6cc69714847e0
Jacob Schwartz <jtschwartz98@gmail.com>
2019-11-26 16:20:46 -0500

    Added UD tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 		<nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
 			<div class="navbar-brand">
 				<a class="navbar-item" href="/">
-					<strong>CPS 452: Emerging Programming Languages</strong>
+					<strong>CPS 452: Emerging Programming Languages - University of Dayton</strong>
 				</a>
 				
 				<div class="navbar-burger">
